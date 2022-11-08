13653850c849bd1f7338a64fd36ebd5cfe421cfa
Peerawit P <jawzainw55@hotmail.com>
2022-11-08 15:40:30 +0700

    👈 Added MainBtnWidget & CreateProductCategory Widget

--- lib/screens/add_product_screen.dart
+++ lib/screens/add_product_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:jengjaew_shop/themes/color.dart';
 import 'package:jengjaew_shop/widgets/input_decoration.dart';
+import 'package:jengjaew_shop/widgets/main_btn_widget.dart';
 
 class AddProductScreen extends StatefulWidget {
   const AddProductScreen({super.key});
@@ -58,12 +59,64 @@ class _AddProductScreenState extends State<AddProductScreen> {
             Form(
                 key: formKey,
                 child: Column(children: [
+                  CreateProductCategory(),
                   CreateProductName(),
                   CreateProductPrice(),
                   CreateProductQuantity(),
                   CreateProductDescription()
                 ])),
+            Expanded(
+                child: MainBtnWidget(
+                    colorBtn: kColorsPurple,
+                    textBtn: 'Confirm',
+                    isTransparent: false,
+                    haveIcon: false)),
           ],
+        ));
+  }
+
+  Widget CreateProductCategory() {
+    List<String> category = [
+      'Pen',
+      'Book',
+      'Paper',
+      'Eraser',
+      'Marker',
+      'Folder'
+    ];
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+        child: InputDecorator(
+          decoration: InputDecoration(
+              contentPadding:
+                  new EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+              border: new OutlineInputBorder(
+                borderRadius: new BorderRadius.circular(10),
+                borderSide: BorderSide(color: kColorsGrey, width: 1),
+              ),
+              errorStyle: Theme.of(context).textTheme.bodyText2),
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton(
+                value: productCategory,
+                icon: SvgPicture.asset('assets/icons/down.svg',
+                    color: kColorsGrey),
+                elevation: 3,
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                    color: kColorsPurple),
+                items: category.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    productCategory = value.toString();
+                  });
+                }),
+          ),
         ));
   }
 
@@ -88,7 +141,10 @@ class _AddProductScreenState extends State<AddProductScreen> {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
       child: TextFormField(
-          keyboardType: TextInputType.text,
+          keyboardType: TextInputType.number,
+          inputFormatters: <TextInputFormatter>[
+            FilteringTextInputFormatter.digitsOnly
+          ],
           autofocus: true,
           style: Theme.of(context).textTheme.subtitle1,
           validator: (value) {
