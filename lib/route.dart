7bb57faae9426cc3c6b9504cfe1a9784cc672648
Peerawit P <jawzainw55@hotmail.com>
2022-11-08 14:32:37 +0700

    📱 Added add_product_screen.dart

--- lib/route.dart
+++ lib/route.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:jengjaew_shop/screens/add_product_screen.dart';
 import 'package:jengjaew_shop/screens/home_screen.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-  "/home": (BuildContext context) => HomeScreen()
+  '/home': (BuildContext context) => HomeScreen(),
+  '/add_product': (BuildContext context) => AddProductScreen(),
 };
